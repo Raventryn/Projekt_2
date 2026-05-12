194b35fc46f39f29a67cda7232841cc8c0d53035
HerbeAmAbgehen <sebastian-loch@outlook.de>
2026-05-12 19:42:42 +0200

    Update HUD
    Added Dummy Code Lines Generator for HUD

---
+++ UNITY/Projekt_2/Assets/_SCRIPTS/UI/HUDCodeLines.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using Ink.Parsed;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[ExecuteInEditMode]
+public class HUDCodeLines : MonoBehaviour
+{
+    [SerializeField] TMP_Text Textfield;
+    [SerializeField] String allSymbols;
+
+    List<Char> symbols = new List<char>();
+
+    void Start()
+    {
+        AddCharsToList();
+
+        InvokeRepeating("GenerateSymbolSequences", 0, 0.1f);
+    }
+
+    void AddCharsToList()
+    {
+        foreach(char symbol in allSymbols)
+        {
+            symbols.Add(symbol);
+        }
+    }
+
+    int GenerateRandomIndex()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, symbols.Count);
+
+        return randomIndex;
+    }
+
+    void GenerateSymbolSequences()
+    {
+        Textfield.text = "";
+
+        for(int i = 0; i < UnityEngine.Random.Range(5,12); i++)
+        {
+            int index = GenerateRandomIndex();
+
+            Textfield.text += symbols[index];
+        }    
+
+        Textfield.text += "\n";
+
+        for(int i = 0; i < UnityEngine.Random.Range(2,7); i++)
+        {
+            int index = GenerateRandomIndex();
+
+            Textfield.text += symbols[index];
+        }
+
+        Textfield.text += "\n";
+
+        for(int i = 0; i < UnityEngine.Random.Range(3,8); i++)
+        {
+            int index = GenerateRandomIndex();
+
+            Textfield.text += symbols[index];
+        }
+
+        Textfield.text += "\n";
+
+        for(int i = 0; i < UnityEngine.Random.Range(2,5); i++)
+        {
+            int index = GenerateRandomIndex();
+
+            Textfield.text += symbols[index];
+        }
+  
+    }
+}
